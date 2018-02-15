4104e5f37dd0803d73b8ca57234f3394b4a9b4ba
aman.yadav <amanyadav2480@gmail.com>
2018-02-15 23:46:35 +0530

    init

---
+++ src/main/java/net/androidsrc/starter/StarterApplication.java
@@ -0,0 +1,19 @@
+package net.androidsrc.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class StarterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StarterApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+}
